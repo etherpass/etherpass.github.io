decbd5fee2d570132f6fe631f9f5c2787ad7c109
Aleksandr Skakovskiy <optimusway@gmail.com>
2018-09-07 03:14:36 +0500

    Add roadmap activities

--- components/Roadmap/Roadmap.jsx
+++ components/Roadmap/Roadmap.jsx
@@ -9,33 +9,45 @@ export default () => (
     <NavHeader name="roadmap">Roadmap</NavHeader>
     <Timeline>
       <TimelineItem
-        title="Pre-ICO"
-        desc="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Leo a diam sollicitudin tempor id eu nisl nunc."
+        title="Whitepaper"
+        desc="Prepare technical documentation."
         date="Q4 2018"
         icon={<FaCoins color="white" />}
       />
       <TimelineItem
-        title="Testnet Launch"
-        desc="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Leo a diam sollicitudin tempor id eu nisl nunc."
+        title="Testnet launch"
+        desc="Alpha version of smart contracts. Initial launch on the blockchain testnet."
         date="Q1 2019"
         icon={<FaServer color="white" />}
       />
       <TimelineItem
-        title="Pre-ICO"
-        desc="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Leo a diam sollicitudin tempor id eu nisl nunc."
-        date="2017-2019"
+        title="ICO"
+        desc="Company registration, legal activities."
+        date="Q2 2019"
+        icon={<FaCoins color="white" />}
+      />
+      <TimelineItem
+        title="MVP"
+        desc="Contracts + desktop client."
+        date="Q3 2019"
         icon={<FiActivity color="white" />}
       />
       <TimelineItem
-        title="Pre-ICO"
-        desc="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Leo a diam sollicitudin tempor id eu nisl nunc."
-        date="2017-2019"
+        title="Full-fledged apps ecosystem"
+        desc="Browser extensions, mobile apps."
+        date="Q4 2019"
         icon={<FiActivity color="white" />}
       />
       <TimelineItem
-        title="Pre-ICO"
-        desc="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Leo a diam sollicitudin tempor id eu nisl nunc."
-        date="2017-2019"
+        title="PKDI"
+        desc="Private key distribution infrastructure."
+        date="Q1 2020"
+        icon={<FiActivity color="white" />}
+      />
+      <TimelineItem
+        title="Passchain"
+        desc="Opinionated secure storage powered by Distributed Ledger Technology (DLT)."
+        date="Q1 2021"
         icon={<FiActivity color="white" />}
       />
     </Timeline>
